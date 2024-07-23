a98b78ff6363363b636c802083557da93a281f53
haseebt456 <m.haseebtufail@gmail.com>
2024-07-23 14:28:25 +0500

    Added a card

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import FlatCards from './components/FlatCards'
 
 const App = () => {
   return (
     <SafeAreaView>
-      <ScrollView>
-      <Text>App</Text>
-      </ScrollView>
+      <Text>
+      <FlatCards />
+      </Text>
     </SafeAreaView >
   )
 }
