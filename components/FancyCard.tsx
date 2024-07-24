e8e469c9eb3b4c4819ba506df58646b7c9d9a0fc
haseebt456 <m.haseebtufail@gmail.com>
2024-07-24 16:39:04 +0500

    Add image

--- components/FancyCard.tsx
+++ components/FancyCard.tsx
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function FancyCard() {
   return (
     <View>
-      <Text>FancyCard</Text>
+      <Text style={styles.headingText}>Trending Places</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image
+        src={'https://upload.wikimedia.org/wikipedia/commons/c/c9/Reflection_of_Farah_Baksh_Terrace_%28Upper_Terrace%29_main_building.jpg'
+        }
+        style={styles.cardImage}
+        />
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+      },
+      card:{},
+      cardElevated:{},
+      cardImage: {
+        height:180   
+      }
+})
